75bd8a1b6bc0b82e37dc86a24c23d0d52f1f5398
Roger Rezende <javielrezende@gmail.com>
2022-01-07 22:03:08 -0300

    :bricks: Add enviroment to the project
    Add enviroment to the project

---
+++ docker/mysql/init_db.sql
@@ -0,0 +1,12 @@
+-- DROP TABLE IF EXISTS `users`;
+
+-- CREATE TABLE `users` (
+--   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+--   `name` varchar(255) NOT NULL,
+--   `email` varchar(255) NOT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- INSERT INTO `users` (name, email) VALUES
+-- ('Roger Rezende 1','roger.rezende1@gmail.com'),
+-- ('Roger Rezende 2','roger.rezende2@gmail.com');
